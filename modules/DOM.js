ff58cd6f582b9839b2303ab9d9bf247e33fa024b
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-06 22:17:32 +0500

    DOM Practice

---
+++ modules/DOM.js
@@ -0,0 +1,9 @@
+// Single Elemnt Selectors
+
+// const list = document.querySelector('#list');
+const listItems =document.querySelectorAll('.list-item')
+
+list.lastElementChild.style.background = 'red';
+// listItems.forEach(item => {
+//   console.log(item.textContent);
+// });
