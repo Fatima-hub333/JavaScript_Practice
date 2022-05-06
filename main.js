271f3d656581b3c5b722c15fc63c73a494ff0d08
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-06 18:58:46 +0500

    Create Array of Objects

---
+++ main.js
@@ -0,0 +1,46 @@
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    pets: ['Lion', 'Cat'],
+    address: {
+      street: '123 Street',
+      city: 'NewYork',
+      state: 'Texas',
+    },
+  },
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    pets: ['Lion', 'Cat'],
+    address: {
+      street: '123 Street',
+      city: 'NewYork',
+      state: 'Texas',
+    },
+  },
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    pets: ['Lion', 'Cat'],
+    address: {
+      street: '123 Street',
+      city: 'NewYork',
+      state: 'Texas',
+    },
+  },
+  {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    pets: ['Lion', 'Cat'],
+    address: {
+      street: '123 Street',
+      city: 'NewYork',
+      state: 'Texas',
+    },
+  },
+];
