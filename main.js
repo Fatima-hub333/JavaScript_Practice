ef3aa1e529b07729e2ac93bbd4404a8e8d6f0691
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-06 20:52:42 +0500

    Practice on functions

--- main.js
+++ main.js
@@ -1,46 +1,64 @@
-const people = [
-  {
-    firstName: 'John',
-    lastName: 'Smith',
-    age: 30,
-    pets: ['Lion', 'Cat'],
-    address: {
-      street: '123 Street',
-      city: 'NewYork',
-      state: 'Texas',
-    },
-  },
-  {
-    firstName: 'John',
-    lastName: 'Smith',
-    age: 30,
-    pets: ['Lion', 'Cat'],
-    address: {
-      street: '123 Street',
-      city: 'NewYork',
-      state: 'Texas',
-    },
-  },
-  {
-    firstName: 'John',
-    lastName: 'Smith',
-    age: 30,
-    pets: ['Lion', 'Cat'],
-    address: {
-      street: '123 Street',
-      city: 'NewYork',
-      state: 'Texas',
-    },
-  },
-  {
-    firstName: 'John',
-    lastName: 'Smith',
-    age: 30,
-    pets: ['Lion', 'Cat'],
-    address: {
-      street: '123 Street',
-      city: 'NewYork',
-      state: 'Texas',
-    },
-  },
-];
+// Objects Literal
+
+// const people = [
+//   {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     age: 30,
+//     pets: ['Lion', 'Cat'],
+//     address: {
+//       street: '123 Street',
+//       city: 'NewYork',
+//       state: 'Texas',
+//     },
+//   },
+//   {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     age: 30,
+//     pets: ['Lion', 'Cat'],
+//     address: {
+//       street: '123 Street',
+//       city: 'NewYork',
+//       state: 'Texas',
+//     },
+//   },
+//   {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     age: 30,
+//     pets: ['Lion', 'Cat'],
+//     address: {
+//       street: '123 Street',
+//       city: 'NewYork',
+//       state: 'Texas',
+//     },
+//   },
+//   {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     age: 30,
+//     pets: ['Lion', 'Cat'],
+//     address: {
+//       street: '123 Street',
+//       city: 'NewYork',
+//       state: 'Texas',
+//     },
+//   },
+// ];
+
+// const peopleJSON = JSON.stringify(people);
+
+// console.log(JSON.parse(peopleJSON));
+
+// Functions
+
+// function hello(firstName, lastName) {
+//   console.log(`Hello ${firstName} ${lastName}`);
+// }
+
+// hello('John', 'Smith');
+
+const convertPercent = num => num/100;
+
+console.log (convertPercent(75));
