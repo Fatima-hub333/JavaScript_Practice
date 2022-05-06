1c71993ee84e9eb91fcffe38a045c4b96632e8e0
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-06 21:16:50 +0500

    Practice on Loop

--- main.js
+++ main.js
@@ -59,6 +59,41 @@
 
 // hello('John', 'Smith');
 
-const convertPercent = num => num/100;
+// const convertPercent = num => num/100;
 
-console.log (convertPercent(75));
+// console.log (convertPercent(75));
+
+// Loops
+
+// let i = 0;
+
+// while ( i < 10 ) {
+//   console.log(i);
+//   i = i + 1;
+// }
+
+// for (let i =0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// const people = [
+//   {
+//     firstName: 'John',
+//     lastName: 'Smith',
+//     age: 30
+//   },
+//   {
+//     firstName: 'Fatima',
+//     lastName: 'Zahir',
+//     age: 25
+//   }
+// ];
+
+// people.forEach(person => console.log(person.age));
+
+// const personName = people.map(person => person.firstName);
+// console.log(personName)
+
+// const personAgeUnder30 = people.filter(person => person.age < 30);
+
+// console.log(personAgeUnder30);
