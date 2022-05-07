f8943e56e7ab486183b91097099bce3080f6dc83
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-07 10:16:12 +0500

    Fix linter errors

--- modules/DOM.js
+++
@@ -1,9 +0,0 @@
-// Single Elemnt Selectors
-
-// const list = document.querySelector('#list');
-const listItems = document.querySelectorAll('.list-item');
-
-list.lastElementChild.style.background = 'red';
-// listItems.forEach(item => {
-//   console.log(item.textContent);
-// });
--- modules/events.js
+++
@@ -1,7 +0,0 @@
-// Events
-
-const btn = document.getElementById('myBtn');
-btn.addEventListener('mouseover', (e) => {
-  btn.style.background = 'blue';
-  console.log(e);
-});
